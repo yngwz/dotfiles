f27c7a4b59f5a19b2cd7a0191dc5b45c530b73cc
yngwz <tim@timothymartin.dev>
2022-02-22 14:32:58 -0700

    beginning conversion to lua / added catppuccin color scheme

---
+++ nvim/init.lua
@@ -0,0 +1,67 @@
+require('plugins')
+local utils = require('utils')
+
+-- aliases
+local opt = vim.opt  -- global
+local g = vim.g      -- global for let options
+local wo = vim.wo    -- window local
+local bo = vim.bo    -- buffer local
+local fn = vim.fn    -- access vim functions
+local cmd = vim.cmd  -- vim commands
+local api = vim.api
+
+------------------------------------------------------------------------------
+-- General Settings
+------------------------------------------------------------------------------
+opt.hidden = true
+opt.termguicolors = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.exrc = true
+opt.nu = true
+opt.relativenumber = true
+opt.hlsearch = false
+opt.errorbells = false
+opt.undodir = "~/.vim/undodir"
+opt.scrolloff = 10
+opt.incsearch = true
+opt.cmdheight = 2
+opt.updatetime = 50
+opt.signcolumn = "auto"
+opt.clipboard = "unnamed"
+opt.ignorecase = true
+opt.completeopt = 'menuone,noselect'
+
+------------------------------------------------------------------------------
+-- Keymaps
+------------------------------------------------------------------------------
+local map = utils.map
+
+--Remap space as leader key
+map('', '<Space>', '<Nop>', { noremap = true, silent = true })
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+map("n", "<leader>vr", ":source $MYVIMRC<cr>",{noremap = true})  -- Source init.lua
+map("n" , "<leader>ve", ":edit ~/.config/nvim/init.lua<cr> ", { noremap = true })   -- Edit init.lua
+
+-- Substitution and Search
+map("n" , "<leader>sg", ":%s/<C-r><C-w>//g<Left><Left>", { noremap = true })
+
+-- Packer shortcuts
+map("n" , "<leader>pi", ":PackerInstall<cr>", { noremap = true })
+map("n" , "<leader>ps", ":PackerSync<cr>", { noremap = true })
+
+------------------------------------------------------------------------------
+-- Plugins
+------------------------------------------------------------------------------
+require "plugins.configs.treesitter"
+require "plugins.configs.catppuccin"
+require "plugins.configs.cmp"
+
+------------------------------------------------------------------------------
+-- Theme
+------------------------------------------------------------------------------
+vim.cmd[[colorscheme catppuccin]]
---
+++ nvim/lua/plugins/configs/cmp.lua
@@ -0,0 +1,39 @@
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
