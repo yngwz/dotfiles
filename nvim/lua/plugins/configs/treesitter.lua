def67e5ebc2e850b84eeadd0344cea3152c8f1a2
yngwz <tim@timothymartin.dev>
2022-02-28 10:58:01 -0700

    Removed non treesitter formatting

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -1,23 +1,22 @@
-local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+	return
 end
 
-configs.setup {
-  ensure_installed = "maintained",
-  sync_install = false,
-  highlight= {
-        enable = true,
-        disable= { "" },
-        additional_vim_regex_highlighting = true
-  },
-  indent = { enable = false, disable = {'yaml'} },
-  context_commentstring = {
-        enable = true,
-        enable_autocmd = false
- },
- autotag = {
-   enable = true
- }
-
-}
+configs.setup({
+	ensure_installed = "maintained",
+	sync_install = false,
+	highlight = {
+		enable = true,
+		disable = { "" },
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	autotag = {
+		enable = true,
+	},
+})
